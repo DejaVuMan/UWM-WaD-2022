56a1aacbbf7eb9521c215716ada75fc7fdad0207
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-05-28 13:44:50 +0200

    Updated Welcome Splash screen with stack element

--- React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
+++ React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import CircularProgress from '@mui/material/CircularProgress'
+import Stack from '@mui/material/Stack'
 
 const Welcome = (props) => {
     return (
-    <div>
-        <CircularProgress color = "secondary"/>
-    </div>
+        <Stack alignItems="center" style={{color: 'white'}}>
+            <h1>Loading...</h1>
+            <CircularProgress color = "secondary"/>
+        </Stack>
     );
 }
 
