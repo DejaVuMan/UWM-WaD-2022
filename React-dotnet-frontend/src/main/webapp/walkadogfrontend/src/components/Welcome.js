797e3a0299e4cde2ba975fc9d1dc03a1eada8f8e
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-05-28 13:45:32 +0200

    Removed unnecessary imports

--- React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
+++ React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React from "react";
 import CircularProgress from '@mui/material/CircularProgress'
 import Stack from '@mui/material/Stack'
 
