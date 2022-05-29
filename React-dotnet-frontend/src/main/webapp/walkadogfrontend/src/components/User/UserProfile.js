731130d2c49e0cd0db7e3fca2a12c9ec3f8d03b1
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-05-29 23:36:00 +0200

    Filled out most of UserProfile but holy cow is React the worst thing ever

--- React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/User/UserProfile.js
+++ React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/User/UserProfile.js
@@ -1,16 +1,96 @@
-import React from "react";
-import {useParams} from 'react-router-dom';
-import {Alert} from "react-bootstrap";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+//import {useParams} from 'react-router-dom';
+import {fetchTrainersAndDataById} from "../../services/index";
 
-const UserProfile = () => {
-    const params = useParams();
-    console.log("UserProfile call")
-    console.log(params)
-    
-return(
-    <Alert style={{opacity: 0.9, padding: '50px',backgroundColor: 'cadetblue', color: "#ffffff", fontFamily: "Montserrat",
-        fontWeight: "500px",textAlign: "center", fontSize:"28px"}}>Witaj  {params.id}</Alert>
-)
+import { CardContent, CardHeader, CardMedia } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card'
+
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        };
+    }
+
+
+
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        if(id)
+        {
+            this.props.fetchTrainersAndDataById(this.props.match.params.id);
+        }
+    }
+
+    render() {
+        const theme = createTheme({
+            palette: {
+                mode: 'dark',
+                primary: {
+                  main: '#3f51b5',
+                  secondary: '#D3D3D3',
+                },
+                secondary: {
+                  main: '#f50057',
+                },
+              },
+            typography: {
+                color: '#3f51b5'
+              }
+          });
+
+        const trainersSum = this.props.trainersind;
+        const trainersIndividual = trainersSum.users;
+
+        const trainersDataInd = trainersSum.trainerdata;
+        console.log("Displaying array data from UserProfile")
+        console.log(trainersSum)
+        console.log(trainersIndividual)
+        console.log(trainersDataInd)
+
+        const Item = styled(Paper)(({ theme }) => ({
+            backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+            ...theme.typography.body2,
+            padding: theme.spacing(1),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+          })); 
+          return(
+            <ThemeProvider theme={theme}>
+                <Card sx={{minWidth: 275}}>
+                    <CardContent>
+                    </CardContent>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://s3.cfluent.com/cdn-uploads/3524/1497367462-dogs-training-2-1024x683.jpg"
+                        alt="Dog in disguise as a Dog Trainer"
+                    />
+                    <CardContent>
+                        Test
+                    </CardContent>
+                </Card>
+              </ThemeProvider>
+          )
+    }
 }
 
-export default UserProfile;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        trainersind: state.user,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchTrainersAndDataById: (id) => dispatch (fetchTrainersAndDataById(id))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
