25d7878fa37bec9beaa5489fad4d8e799d483f26
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-05-30 21:49:50 +0200

    Added import statement to Button in UserProfile, replaced Welcome.js generic loading symbol with dog gif :)

--- React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/User/UserProfile.js
+++ React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/User/UserProfile.js
@@ -11,6 +11,7 @@ import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider'
 import Stack from '@mui/material/Stack'
 import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
 
 class UserProfile extends Component {
     constructor(props) {
--- React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
+++ React-dotnet-frontend/src/main/webapp/walkadogfrontend/src/components/Welcome.js
@@ -5,8 +5,8 @@ import Stack from '@mui/material/Stack'
 const Welcome = (props) => {
     return (
         <Stack alignItems="center" style={{color: 'white'}}>
+            <img src="https://64.media.tumblr.com/c9168657541398db4d9abf4bb4734c93/tumblr_mic0kpgDVM1rnfi40o1_400.gif" alt="Loading Animation"/>
             <h1>Loading...</h1>
-            <CircularProgress color = "secondary"/>
         </Stack>
     );
 }
