19299f62a251b4c210fa185b3a61b385fa8bff29
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-06-07 20:21:47 +0200

    Updated Actually Updated DB

--- dotnet_walkADog_backend/Helpers/DataContext.cs
+++ dotnet_walkADog_backend/Helpers/DataContext.cs
@@ -21,4 +21,5 @@ public class DataContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<TrainerData> TrainerData { get; set; }
     public DbSet<Dog> Dogs { get; set; }
+    public DbSet<ReservationData> ReservationData { get; set; }
 }
---
+++ dotnet_walkADog_backend/Migrations/SqliteMigrations/20220607182101_ReserveTableActuallyAdd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations.SqliteMigrations
+{
+    public partial class ReserveTableActuallyAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReservationData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    startWindow = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    reservationLength = table.Column<int>(type: "INTEGER", nullable: false),
+                    isReserved = table.Column<bool>(type: "INTEGER", nullable: false),
+                    userId = table.Column<int>(type: "INTEGER", nullable: false),
+                    trainerId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationData", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReservationData");
+        }
+    }
+}
